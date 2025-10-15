4df9934e0d715f90834c7d771df5e318cd88c304
Marliva <cedriclevasseur76@gmail.com>
2025-10-15 11:15:33 +0200

    Etablissement fonction powerPc pour créer aléatoirement pouvoir

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
- 
+function powerPc() {
+  const pouvoirs = ["feu", "eau", "terre"]
+  const indexAleatoire = Math.floor(Math.random() * 3)
+  const choixOrdinateur = pouvoirs[indexAleatoire]
+
+  console.log("Choix de l'ordinateur:", choixOrdinateur)
+  return choixOrdinateur
+}
