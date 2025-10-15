775e8092cc85e2c18a976bf2bf4ae9d846c3ca94
Marliva <cedriclevasseur76@gmail.com>
2025-10-15 11:57:13 +0200

    Ajout des fonctions powerPlayer (récupère le choix du joueur en cliquant sur le bouton), playGame (pour déterminer qui gagne la manche avec une condition), et afficherResultat (pour afficher le résultat de la manche)

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -9,3 +9,47 @@ function powerPc() {
 
 const boutons = document.querySelectorAll(".power-btn")
 
+function powerPlayer(boutonClique) {
+  const choixJoueur = boutonClique.getAttribute("data-power")
+  console.log("Choix du joueur:", choixJoueur)
+
+  return choixJoueur
+}
+
+function playGame(joueur, ordinateur) {
+  let message = ""
+  let typeResultat = ""
+
+  if (joueur === ordinateur) {
+    message = `Égalité ! Vous avez tous les deux choisi l'élément ${joueur} !`
+    typeResultat = "draw"
+  }
+  else if (
+    (joueur === "feu" && ordinateur === "terre") ||
+    (joueur === "eau" && ordinateur === "feu") ||
+    (joueur === "terre" && ordinateur === "eau")
+  ) {
+    message = `Vous avez gagné ! L'élément ${joueur} bat l'élément ${ordinateur} !`
+    typeResultat = "win"
+  }
+  else {
+    message = `Vous avez perdu ! L'élément ${ordinateur} bat l'élément ${joueur} !`
+    typeResultat = "lose"
+  }
+
+  console.log("Résultat:", message)
+
+  return {
+    message: message,
+    type: typeResultat,
+  }
+}
+
+function afficherResultat(resultat, joueur, ordinateur) {
+  const zoneResultat = document.getElementById("result")
+  const zoneChoix = document.getElementById("choices")
+
+  zoneResultat.textContent = resultat.message
+  zoneResultat.classList.remove("win", "lose", "draw")
+  zoneResultat.classList.add(resultat.type)
+}
